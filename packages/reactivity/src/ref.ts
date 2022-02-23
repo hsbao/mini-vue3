a2db0653efcbedfb13e19256064d3097984cfbf9
hongshibao <771989536@qq.com>
2022-02-23 23:32:20 +0800

    实现ref和toRef

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,19 +1,32 @@
+import { isObject } from '@vue/shared'
+import { hasChanged, isArray } from './../../shared/src/index'
+import { track, trigger } from './effect'
+import { TrackOperatorTypes, TriggerOperatorTypes } from './operators'
+import { reactive } from './reactive'
+
 /**
  * 转成es5，就是Object.defineProperty
  * 获取value的时候触发get
  * 设置value的时候触发set
  */
+const convert = val => (isObject(val) ? reactive(val) : val)
 class RefImpl {
   public _value
   public __v_isRef: true
   constructor(public rawValue, public shallow) {
-    this._value = rawValue
+    // 如果是对象，那么就使用reactive变成响应式的
+    this._value = shallow ? rawValue : convert(rawValue)
   }
   get value() {
+    track(this, TrackOperatorTypes.GET, 'value')
     return this._value
   }
   set value(newValue: any) {
-
+    if (hasChanged(this.rawValue, newValue)) {
+      this.rawValue = newValue
+      trigger(this, TriggerOperatorTypes.SET, 'value', newValue)
+      this._value = newValue
+    }
   }
 }
 
@@ -35,17 +48,36 @@ export function shallowRef(value) {
   return createRef(value, true)
 }
 
-/**
- * 把对象里某个属性转成ref，一般是proxy对象上的某个属性
- * @param target 传进来的proxy对象
- * @param key 
- */
-export function toRef(target, key) {
-
+// 因为通常是把proxy中的某个属性转成ref，本身就已经有了getter和setter
+// 已经具备了收集依赖和触发更新的逻辑了，这里就不用加上了
+class ObjectRefImpl {
+  public __v_isRef = true
+  constructor(public target, public key) {}
+  get value() {
+    return this.target[this.key]
+  }
+  set value(newValue) {
+    this.target[this.key] = newValue
+  }
 }
 
 /**
- * 把proxy对象里所有属性转成ref
+ * 把对象里某个属性转成ref，一般是proxy对象上的某个属性
+ * @param target 传进来的proxy对象
+ * @param key
+ */
+export function toRef(target, key) {
+  return new ObjectRefImpl(target, key)
+}
+
+/**
+ * 把proxy对象/数组里所有属性转成ref
  * @param target
  */
-export function toRefs(target) {}
+export function toRefs(target) {
+  const res = isArray(target) ? new Array(target.length) : {}
+  for (let key in target) {
+    res[key] = toRef(target, key)
+  }
+  return res
+}
